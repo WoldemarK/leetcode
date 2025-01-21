24c391757820cb3b09948569d51ff959f21e339a
WoldemarK <kovtynov.vladimir@gmail.com>
2025-01-21 19:55:47 +0300

    new method MergeTwoSortedLists

--- src/main/java/org/example/MergeTwoSortedLists.java
+++ src/main/java/org/example/MergeTwoSortedLists.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * You are given the heads of two sorted linked lists list1 and list2.
  * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
@@ -12,20 +16,29 @@ public class MergeTwoSortedLists {
         System.out.println(mergeTwoLists(l1, l2));
     }
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        Stream<ListNode> stream1 = toStream(list1);
+        Stream<ListNode> stream2 = toStream(list2);
+
         ListNode dummy = new ListNode();
-        ListNode curr = dummy;
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                curr.next = list1;
-                list1 = list1.next;
-            } else {
-                curr.next = list2;
-                list2 = list2.next;
-            }
-            curr = curr.next;
-        }
-        curr.next = list1 == null ? list2 : list1;
+
+        final ListNode[] curr = {dummy};
+
+        Stream.concat(stream1, stream2)
+                .sorted((a, b) -> Integer.compare(a.val, b.val))
+                .forEach(node -> {
+                    curr[0].next = node;
+                    curr[0] = curr[0].next;
+                });
+
         return dummy.next;
+    }
+    private static Stream<ListNode> toStream(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        return list.stream();
     }
 }
 
